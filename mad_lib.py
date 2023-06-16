39d492be27ab293e3093874730d30dc334b6ba4f
USER <muhammaddaffa0606@gmail.com>
2023-06-16 18:34:17 +0700

    update mad_lib commit

--- mad_lib.py
+++ mad_lib.py
@@ -1,16 +1,28 @@
-def get_input(getInput: str):
-    user_input: str = input(f"Enter a {getInput}")
-    return user_input
+import time
+
+class MadLib:
+    def __init__(self):
+        pass
+
+    def get_input(self: str):
+        user_input: str = input(f"Enter a {self}")
+        return user_input
 
 
 # Input String
-input1 = get_input("Your Input-1 : ")
-input2 = get_input("Your Input-2 : ")
+input1 = MadLib.get_input("Your Input-1 : ")
+input2 = MadLib.get_input("Your Input-2 : ")
 
+start_second = time.time()
 result = f"""
 These are your
 Input 1 is {input1}
-Input 2 is {input2}
+Input 1 is {input2}
 """
+end_second = time.time()
 
 print(result)
+
+result_second = start_second - end_second
+
+print(f"{result_second} seconds")
---
+++ number_guess_game.py
@@ -0,0 +1,49 @@
+# from random import randint
+#
+#
+# class NumberGuess:
+#
+#     def __init__(self):
+#         pass
+#
+#     def get_lower_number(self):
+#         pass
+#
+#
+#     def get_higher_number(self):
+#         pass
+#
+#
+#     def get_random_number(self):
+#         pass
+#
+#     def number_time_guess(self, number_guess:int):
+#         return number_guess++
+#
+#
+# # enter_lower_num:int = int(input(f"Enter your LOWER number : "))
+# # enter_higher_num:int = int(input(f"Enter your HIGHER number : "))
+# #
+# # random_number:int = randint(enter_lower_num,enter_higher_num)
+# # print(f"Guess the number in the range from {enter_lower_num} to {enter_higher_num}")
+# #
+# # number_guess = 0
+# #
+# # while True :
+# #     try:
+# #         user_guess:int = int(input(f"Input your number : "))
+# #     except ValueError as e:
+# #         print("Please correct the input")
+# #         continue
+# #
+# #     number_guess+=1
+# #
+# #     if user_guess > random_number:
+# #         print("Your guess is bigger")
+# #     elif user_guess < random_number:
+# #         print("Your guess is lower")
+# #     else:
+# #         print("Congrats, Your guess is correc !!")
+# #         break
+# #
+# #     print(f"You have guess in {number_guess} times !!! ")
