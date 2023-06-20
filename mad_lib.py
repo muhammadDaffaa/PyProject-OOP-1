5f5500b5d0918ae19a5def4c6d88990afae4b681
USER <muhammaddaffa0606@gmail.com>
2023-06-20 11:26:26 +0700

    no update significant

--- mad_lib.py
+++ mad_lib.py
@@ -1,28 +1,22 @@
 import time
 
 class MadLib:
-    def __init__(self):
+    def __init__(self,input1,input2):
+        self.input1 = input1
+        self.input2 = input2
+
+    def get_input(self: str,new_input):
+        user_input: str = input(f"Enter a {new_input}")
+        # return user_input
+
+    def __str__(self:str):
+        result = f"""
+        These are your
+        Input 1 is {self.input1}
+        Input 2 is {self.input2}
+        """
+        return result
+
+    def start_second(self):
         pass
-
-    def get_input(self: str):
-        user_input: str = input(f"Enter a {self}")
-        return user_input
-
-
-# Input String
-input1 = MadLib.get_input("Your Input-1 : ")
-input2 = MadLib.get_input("Your Input-2 : ")
-
-start_second = time.time()
-result = f"""
-These are your
-Input 1 is {input1}
-Input 1 is {input2}
-"""
-end_second = time.time()
-
-print(result)
-
-result_second = start_second - end_second
-
-print(f"{result_second} seconds")
+        # return self.time()
