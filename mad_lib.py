2c217024f2b8b8b7e04e812ea212e7a6751a51ac
USER <muhammaddaffa0606@gmail.com>
2023-06-20 15:45:40 +0700

    no update significant

--- mad_lib.py
+++ mad_lib.py
@@ -1,19 +1,18 @@
 import time
 
 class MadLib:
-    def __init__(self,input1,input2):
-        self.input1 = input1
-        self.input2 = input2
+    def __init__(self):
+        pass
 
     def get_input(self: str,new_input):
         user_input: str = input(f"Enter a {new_input}")
+        self.input1 = user_input
         # return user_input
 
-    def __str__(self:str):
+    def get_print(self:str):
         result = f"""
         These are your
         Input 1 is {self.input1}
-        Input 2 is {self.input2}
         """
         return result
 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import mad_lib
+from mad_lib import MadLib #Import Class from different file
 
 # Call Class
-input1 = mad_lib.MadLib(5,6)
+input1 = MadLib()
+
+input1.get_input("your number : ")
+input1.get_print()
