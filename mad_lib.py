411ca1c07c99d988bfea2181471b7ce55b9c4120
USER <muhammaddaffa0606@gmail.com>
2023-06-21 09:32:21 +0700

    update commit

--- mad_lib.py
+++ mad_lib.py
@@ -2,20 +2,17 @@ import time
 
 class MadLib:
     def __init__(self):
-        pass
+        self.__data_input = '' #Private attribute
 
     def get_input(self: str,new_input):
         user_input: str = input(f"Enter a {new_input}")
-        self.input1 = user_input
+        self.__data_input = user_input
         # return user_input
 
     def get_print(self:str):
         result = f"""
         These are your
-        Input 1 is {self.input1}
+        Input 1 is {self.__data_input}
         """
-        return result
-
-    def start_second(self):
-        pass
-        # return self.time()
+        print(result)
+        # return result
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from mad_lib import MadLib #Import Class from different file
 
 # Call Class
 input1 = MadLib()
+input2 = MadLib()
 
-input1.get_input("your number : ")
+input1.get_input("Your First number : ")
+input2.get_input("Your Second number : ")
 input1.get_print()
+input2.get_print()
